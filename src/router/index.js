cf7accad3ba3a3ab4000bb0637a4bf16f98d16d7
ree-rishun <ree0432@gmail.com>
2021-12-04 21:46:55 +0900

    feature: サインインページの佐久市江

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/dashboard/Home.vue'
+import Signin from '../views/auth/Signin.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin,
   },
 ]
 
--- src/views/auth/Signin.vue
+++ src/views/auth/Signin.vue
@@ -1,13 +1,91 @@
 <template>
-  
+  <div class="signin">
+
+    <h2>SIGNIN</h2>
+
+    <input
+      class=""
+      type="text"
+      v-model="user.id"
+      placeholder="IDを入力してください">
+    <input
+      class=""
+      type="text"
+      v-model="user.password"
+      placeholder="パスワードを入力してください">
+
+    <p class="annotation">
+      会員でない方は<router-link to="/signup">新規登録</router-link>
+    </p>
+
+    <button
+      class="nomal_button"
+      @click="signinSubmit">
+      サインイン
+    </button>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'signin'
+    name: 'signin',
+    data () {
+      return {
+        user: {
+          id: '',
+          password: '',
+        }
+      }
+    },
+    methods: {
+      signinSubmit () {
+        // TODO: サインイン処理を追加
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .signin {
+    min-height: 60vh;
+  }
 
+  h2 {
+    text-align: center;
+    font-weight: bold;
+    font-size: 24px;
+    margin-bottom: 30px;
+  }
+
+  input[type=text] {
+    width: 76%;
+    padding: 0 2%;
+    height: 40px;
+    margin: 30px 0 0 10%;
+    border-radius: 5px;
+    font-weight: bolder;
+  }
+  
+  .annotation {
+    width: 80%;
+    margin: 20px 0 0 10%;
+
+    :link, :visited {
+      color: #FF6F00;
+    }
+  }
+  
+  .nomal_button {
+    display: block;
+    width: 80%;
+    height: 40px;
+    margin: 60px 0 0 10%;
+    background-color: #FF6F00;
+    border-radius: 10px;
+    font-size: 20px;
+    color: #FFFFFF;
+    font-weight: bold;
+    outline: none;
+    border: none;
+  }
 </style>
